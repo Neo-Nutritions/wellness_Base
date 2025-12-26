6d3cbf5f86e358cfbacd6595addec7e49c6ab9e8
JAPH254 <omanyujapheth@gmail.com>
2025-12-26 11:28:56 +0300

    Stop tracking file.log and add to gitignore

--- userAuth/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-class FirebaseUserSerializer(serializers.Serializer):
-    uid = serializers.CharField()
-    email = serializers.EmailField(required=False, allow_blank=True, allow_null=True)
-    displayName = serializers.CharField(required=False, allow_blank=True, allow_null=True)
-    phoneNumber = serializers.CharField(required=False, allow_blank=True, allow_null=True)
-    fullName = serializers.CharField(required=False, allow_blank=True, allow_null=True)
-    phone = serializers.CharField(required=False, allow_blank=True, allow_null=True)
