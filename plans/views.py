08fbe2925efc73c6d6802234cbdfdecb51c1801e
JAPH254 <omanyujapheth@gmail.com>
2026-01-21 08:45:33 +0300

    feat:Add SubscriptionPlanListView API endpoint for public subscription plans

--- plans/views.py
+++ plans/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics 
+from plans.models import SubscriptionPlan
+from plans.serializers import SubscriptionPlanSerializer
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class SubscriptionPlanListView(generics.ListAPIView):
+    """
+    API view to list all active subscription plans.
+    """
+    queryset = SubscriptionPlan.objects.filter(is_active=True, is_public=True).order_by('sort_order', 'price')
+    serializer_class = SubscriptionPlanSerializer
+    permission_classes = [AllowAny]
