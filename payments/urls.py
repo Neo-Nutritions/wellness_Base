2feac929035b69a86b44eefc7a65d11bf9e36832
JAPH254 <omanyujapheth@gmail.com>
2026-01-27 23:20:00 +0300

    feat: add Mpesa callback URL endpoint

---
+++ payments/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import MpesaCallbackView
+
+urlpatterns = [
+    path(
+        "mpesa/callback/",
+        MpesaCallbackView.as_view(),
+        name="mpesa-callback",
+    ),
+]
