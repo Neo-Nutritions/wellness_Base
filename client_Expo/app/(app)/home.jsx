ec1bf6438bbb9a2c0349352f21dc6d596490ade1
JAPH254 <omanyujapheth@gmail.com>
2026-01-28 03:04:49 +0300

    Add responsive design and keyboard handling to home screen

--- client_Expo/app/(app)/home.jsx
+++ client_Expo/app/(app)/home.jsx
@@ -1,9 +1,15 @@
 import { View, Text, Pressable } from 'react-native';
-import React,{useState,useEffect, use} from 'react';
+import React, { useState, useEffect, use } from 'react';
 import { useAuth } from '@/context/authContext';
 import '../../global.css';
+import CustomKeyboardView from '@/components/CustomKeyboardView';
 import { useRouter } from 'expo-router';
-import {getCurrentUser} from '@/services/api';
+import { getCurrentUser } from '@/services/api';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
 export default function home() {
   const { logout } = useAuth();
   const router = useRouter();
@@ -18,17 +24,19 @@ export default function home() {
       }
     };
     fetchUserProfile();
-  }, [])
+  }, []);
   return (
     <View>
-      <Text>home</Text>
-      <Text>Full Name {user?.full_name}</Text>
-      <Text>Email Address {user?.email}</Text>
-      <Text>Phone Number {user?.phoneNumber}</Text>
-      <Text>Email is verified {user?.emailVerified}</Text>
-      <Text onPress={logout}>logout</Text>
-      <Text onPress={() => router.push('/')}>Back to landing</Text>
-      <Text onPress={() => router.push('/plans')}>Go to plans</Text>
+      <View style={{ padding: wp('5%') }}>
+        <Text>home</Text>
+        <Text>Full Name {user?.full_name}</Text>
+        <Text>Email Address {user?.email}</Text>
+        <Text>Phone Number {user?.phoneNumber}</Text>
+        <Text>Email is verified {user?.emailVerified}</Text>
+        <Text onPress={logout}>logout</Text>
+        <Text onPress={() => router.push('/')}>Back to landing</Text>
+        <Text onPress={() => router.push('/plans')}>Go to plans</Text>
+      </View>
     </View>
   );
 }
